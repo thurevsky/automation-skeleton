1bcc2af70145c03b521e38e9f046be9298fe9ac7
Sahid Nur afrizal <sahidnurafrizal@Sahids-MacBook-Pro.local>
2019-07-24 17:11:59 +0700

    Tagged for v0.0.1 as working code

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
